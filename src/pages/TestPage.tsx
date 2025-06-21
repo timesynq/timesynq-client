87d2ce1f1acbcbd85c553adda1fd110fa4749da0
pete <ptgh163@gmail.com>
2025-06-20 19:18:45 -0700

    vite setup

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,16 @@
+const TestPage = () => {
+    return (
+        <main className="flex">
+            <section className="text-right">
+                <h1 className="text-3xl font-bold underline">
+                    tymesynq landing page
+                </h1>
+            </section>
+            <footer className="absolute bottom-4 text-sm text-gray-500">
+                copyright timesynq
+            </footer>
+        </main>
+    );
+}
+
+export default TestPage;
