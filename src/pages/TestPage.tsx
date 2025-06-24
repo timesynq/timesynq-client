0a56520d42826d320b324a074824d1e6f11d3532
pete <ptgh163@gmail.com>
2025-06-23 17:51:14 -0700

    Fixed node packages

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -3,7 +3,7 @@ const TestPage = () => {
         <main className="flex">
             <section className="text-right">
                 <h1 className="text-3xl font-bold underline">
-                    tymesynq landing page
+                    timesynq landing page
                 </h1>
             </section>
             <footer className="absolute bottom-4 text-sm text-gray-500">
