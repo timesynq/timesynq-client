2c6bf5f88290797e63863940c5ca09d3faae3ccf
Phillip Grazhdan <ptgh163@gmail.com>
2025-07-04 17:38:31 -0700

    Added new page routes

---
+++ src/pages/public-profile.tsx
@@ -0,0 +1,16 @@
+const PublicProfile = () => {
+    return (
+        <main className="flex">
+            <section className="text-right">
+                <h1 className="text-3xl font-bold underline">
+                    public profile
+                </h1>
+            </section>
+            <footer className="absolute bottom-4 text-sm text-gray-500">
+                copyright timesynq
+            </footer>
+        </main>
+    );
+}
+
+export default PublicProfile
