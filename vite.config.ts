065bba6578a756fadbd2f5f449f69fe0f03321b1
4acf <acfree332211@gmail.com>
2025-09-08 15:22:40 -0700

    add config files + shadcn/ui components

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,5 +6,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()]
+  plugins: [react(), tailwindcss(), basicSsl()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
