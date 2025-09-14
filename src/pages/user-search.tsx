55bcf86cc926c2754658bebabee5c65780e65da6
Raccoon-Man-P <ptgh163@gmail.com>
2025-09-13 19:25:08 -0700

    Various frontend additions

--- src/pages/user-search.tsx
+++ src/pages/user-search.tsx
@@ -2,29 +2,31 @@ import { NavBar } from "@/components/nav-bar";
 import { useAuthStore } from "@/hooks/use-auth-store";
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 const UserSearch = () => {
-
-    const { user, fetchUser } = useAuthStore();
+    const { user, isLoading, fetchUser } = useAuthStore();
 
     useEffect(() => {
         fetchUser();
     }, [fetchUser]);
 
+    if (!user && !isLoading) {
+        return <Navigate to="/" state={{ open: "signin" }} replace />;
+    }
+
     return (
-    <>
-        {user ? (
         <>
             <NavBar />
             <main className="flex flex-col items-center justify-center">
-            {user.userName}
+                {isLoading ? (
+                    <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                ) : (
+                    <p>User Search</p>
+                )}
             </main>
         </>
-        ) : (
-            <Navigate to="/" state={{ open: "signin" }} replace />
-        )}
-    </>
     );
 }
 
