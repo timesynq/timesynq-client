3a7cfdbcc4d37ffe639eb6aabe2a59d2553a13d8
Raccoon-Man-P <ptgh163@gmail.com>
2025-09-19 16:25:10 -0700

    Sandbox environment for isolated component testing

---
+++ src/tracker/tracker-page.tsx
@@ -0,0 +1,21 @@
+import { useAuthStore } from "@/hooks/use-auth-store";
+import { useEffect } from "react";
+
+export const Tracker = () => {
+
+    const { user, fetchUser } = useAuthStore();
+
+    useEffect(() => {
+        fetchUser();
+    }, [fetchUser]);
+
+    return (
+        <div className="flex flex-col min-h-screen">
+            <main className="flex-1 flex flex-col items-center justify-center m-4">
+                {user?.userName}
+                <p>Tracker</p>
+            </main>
+        </div>
+    );
+
+}
