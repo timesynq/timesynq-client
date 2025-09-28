78dcd33f13daead8761091eaeaac41f49c2e0d18
4acf <acfree332211@gmail.com>
2025-09-28 15:30:55 -0700

    fix email confirmed page flash

--- src/pages/email-confirmed.tsx
+++ src/pages/email-confirmed.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const EmailConfirmed = () => {
     return (
         <main className="flex flex-col items-center m-4">
-            <Card>
+            <Card style={{backgroundColor: "oklch(20.019% 0.04696 287.092)", border: "1px solid oklch(1 0 0 / 10%)"}}>
                 <CardContent className="flex flex-col items-center justify-center space-y-2 p-8 w-[400px]">
                     <p>Thank you for confirming your email!</p>
                     <Link to="/">
